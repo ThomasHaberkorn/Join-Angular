88c109a95c0a217adbcec7ea0d54cdb0f40ff66e
ThomasHaberkorn <126246580+ThomasHaberkorn@users.noreply.github.com>
2024-10-05 00:02:30 +0200

    contacts first edit

--- src/app/dashboard/users/users.component.ts
+++ src/app/dashboard/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { collection, Firestore, getDoc, getDocs } from '@angular/fire/firestore';
+import { User } from '../../../models/user.class';
 
 @Component({
   selector: 'app-users',
@@ -8,5 +10,25 @@ import { Component } from '@angular/core';
   styleUrl: './users.component.scss'
 })
 export class UsersComponent {
+  users: User[] = [];
 
+  constructor(private firestore: Firestore) { }
+
+  async ngOnInit() {
+    await this.loadUsersFromFirestore();
+  }
+
+  async loadUsersFromFirestore() {
+    try {
+      const querySnapshot = await getDocs(collection(this.firestore, 'users'));
+      this.users = querySnapshot.docs.map(doc => doc.data() as User);
+      console.log('Users loaded:', this.users);
+    } catch (error) {
+      console.error('Error loading users:', error);
+    }
+  }
+
+  addContact() {
+    console.log('Contact added');
+  }
 }
